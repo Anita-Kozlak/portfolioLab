406c20d2f8e80fc73f0739d5bbbef41f6c8963e3
Anita-Kozlak <anita.kozlak@gmail.com>
2020-11-24 19:24:02 +0100

    modified styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -120,7 +120,7 @@ const Contact = () => {
                     message: 'Wiadomosć musi mieć conajmniej 120 znaków!',
                   },
                 })}
-                rows={6}
+                rows={7}
                 type="text"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
