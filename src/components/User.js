1726c316569c36ad028919fd98341afb795347ca
Anita-Kozlak <anita.kozlak@gmail.com>
2020-11-29 11:32:40 +0100

    modified User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,31 +1,14 @@
-import React, { useState } from 'react';
-import firebase from '../components/firebase';
-// import Delete from '@material-ui/icons/Delete';
-// import EditIcon from '@material-ui/icons/Edit';
+import React from 'react';
 export default function User({ user, index}) {
   
-//     const deleteUser = () => {
-//     const userRef = firebase.database().ref('users').child(user.email)
-//     userRef.remove();
-   
-//     }
 
-    
-//    const editUser = () => {
-//    const giftRef = firebase.database().ref('users');
-//    giftRef.set({
-    //  email: email,
-//    });
-    
 
-   };
    return (
      <>
        <div>
          <h3>
            {index + 1}.{user}
-           {/* <Delete onClick={deleteUser}>Usuń</Delete>
-           <EditIcon onClick={editUser} Edycja></EditIcon> */}
+        
          </h3>
        </div>
      </>
