c0444db99a0fc5f5cc48147f0ffe7227986433a1
Anita-Kozlak <anita.kozlak@gmail.com>
2020-11-29 11:26:41 +0100

    modified Users.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
 import firebase from '../components/firebase';
-import Delete from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+// import Delete from '@material-ui/icons/Delete';
+// import EditIcon from '@material-ui/icons/Edit';
 export default function User({ user, index}) {
   
-    const deleteUser = () => {
-    const userRef = firebase.database().ref('users').child(user.email)
-    userRef.remove();
-    console.log(user.email)
+//     const deleteUser = () => {
+//     const userRef = firebase.database().ref('users').child(user.email)
+//     userRef.remove();
    
-    }
+//     }
 
     
-   const editUser = () => {
-   const giftRef = firebase.database().ref('users');
-   giftRef.set({
-     email: email,
-   });
+//    const editUser = () => {
+//    const giftRef = firebase.database().ref('users');
+//    giftRef.set({
+    //  email: email,
+//    });
     
 
    };
@@ -25,8 +24,8 @@ export default function User({ user, index}) {
        <div>
          <h3>
            {index + 1}.{user}
-           <Delete onClick={deleteUser}>Usuń</Delete>
-           <EditIcon onClick={editUser} Edycja></EditIcon>
+           {/* <Delete onClick={deleteUser}>Usuń</Delete>
+           <EditIcon onClick={editUser} Edycja></EditIcon> */}
          </h3>
        </div>
      </>
