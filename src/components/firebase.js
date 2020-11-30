6e59734cae44248f5f61e869c89e9b45f96614ca
Anita-Kozlak <63249604+Anita-Kozlak@users.noreply.github.com>
2020-11-30 16:25:16 +0100

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,15 +3,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const config = {
-    apiKey: "AIzaSyAvMAvrVlEZvcdDyoqafpn8iITnh-a51I0",
-    authDomain: "portfoliolab-c9282.firebaseapp.com",
-    databaseURL: "https://portfoliolab-c9282.firebaseio.com",
-    projectId: "portfoliolab-c9282",
-    storageBucket: "portfoliolab-c9282.appspot.com",
-    messagingSenderId: "829780221566",
-    appId: "1:829780221566:web:e6e3be2394f5ea6e74822f",
-    measurementId: "G-5N5E2TG4SL"
-  };
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+ };
 
   const firebase = app.initializeApp(config)
   
